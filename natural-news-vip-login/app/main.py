2f7af84d24a844feb7fe896b89278b12d97b293c
Rabb <rabb.pham@nfcguest.com>
2025-03-22 10:17:29 +0700

    add cors

--- natural-news-vip-login/app/main.py
+++ natural-news-vip-login/app/main.py
@@ -2,7 +2,16 @@ from fastapi import FastAPI
 from app.database import engine, Base
 from  app.api.routes.users import router as user_router
 from app.api.routes.chats import router as chat_router
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Cho phép tất cả nguồn gốc (dành cho testing)
+    allow_credentials=True,
+    allow_methods=["*"],  # Cho phép tất cả HTTP methods (GET, POST, PUT, DELETE, v.v.)
+    allow_headers=["*"],  # Cho phép tất cả headers
+)
 
 # Create database tables
 Base.metadata.create_all(bind=engine)
